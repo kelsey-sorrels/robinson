40241ad58b7c7acbc57af995adb98121c0957d82
Aaron Santos <aaronds109@gmail.com>
2013-02-13 07:46:43 -0800

    Create framework for reloadable cljs

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dungeon-crusade "0.0.1-SNAPSHOT"
+  :description "dungeon-crusade"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ns-tracker "0.2.1"]]
+  :main dungeon-crusade.core
+  :repl-init dungeon-crusade.core)
---
+++ src/dungeon_crusade/core.clj
@@ -0,0 +1,47 @@
+(ns dungeon-crusade.core
+  (:use ns-tracker.core)
+  (:require dungeon-crusade.main))
+
+(defn check-namespace-changes [track]
+ (try
+   (doseq [ns-sym (track)]
+     (println "Reloading namespace:" ns-sym)
+     (require ns-sym :reload))
+   (catch Throwable e (.printStackTrace e)))
+   (Thread/sleep 500))
+
+(defn start-nstracker []
+ (let [track (ns-tracker ["src" "checkouts"])]
+   (doto
+     (Thread.
+       #(while true
+         (check-namespace-changes track)))
+     (.setDaemon true)
+     (.start))))
+
+(defn -main []
+  (do 
+    ;; use all symbols from all namespaces except some.
+    (doseq [ns (map symbol (filter #(not (or (.startsWith % "clojure")
+                                             (.startsWith % "complete")
+                                             (.startsWith % "reply")
+                                             (.startsWith % "user")))
+                                   (map str (all-ns))))]
+      (use ns))
+    (let [default-setup-fn (constantly {})
+          default-tick-fn  (fn [state] (do (println "default tick fn") (Thread/sleep 5000) state))
+          get-setup-fn     (fn [] (if-let [f (resolve 'setup)] f default-setup-fn))
+          get-tick-fn      (fn [] (if-let [f (resolve 'tick)] f default-tick-fn))]
+      (start-nstracker)
+      ; up setup fn changes, this loop will restart
+      (loop [setup-fn (get-setup-fn)]
+        (let [setup-fn-var (var-get setup-fn)]
+          (println "(Re)starting loop with new setup-fn")
+          ; start with initial state from setup-fn
+          (loop [state ((get-setup-fn))]
+            ; setup function changed? stop ticking
+            (when (identical? (var-get (get-setup-fn)) setup-fn-var)
+              ; tick the old state through the tick-fn to get the new state
+              (recur ((get-tick-fn) state)))))
+          ; setup function changed, start outer loop over again
+        (recur (get-setup-fn))))))

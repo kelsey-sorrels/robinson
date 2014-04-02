881c936db9d8868d7f360e2fc911d736d44bcc66
Kelsey Sorrels <kelsey.d.sorrels@gmail.com>
2014-04-01 21:05:21 -0700

    Add standalone mapgen

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "dungeon-crusade"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ns-tracker "0.2.2"]
+                 [org.clojure/math.combinatorics "0.0.7"]
+                 [org.clojars.vishk/algotools "0.1.0"]
                  [org.clojars.folcon/clojure-lanterna "0.9.5-SNAPSHOT"]]
   :main dungeon-crusade.core
   :repl-init dungeon-crusade.main)

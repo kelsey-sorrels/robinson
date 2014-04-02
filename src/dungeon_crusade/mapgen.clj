0664779d942fe39b76898013f4ae4196d1789a7b
Aaron Santos <aaronds109@gmail.com>
2014-04-01 21:05:21 -0700

    Add standalone mapgen

--- src/dungeon_crusade/mapgen.clj
+++ src/dungeon_crusade/mapgen.clj
@@ -15,31 +15,38 @@
    :width  (+ 2 (rand-int 5))
    :height (+ 2 (rand-int 5))})
 
-(defn rooms-to-graph [rooms]
-  (let [all-edges (combo/combinations (map :id rooms) 2)
-        minimum-edges (graph/MST-kruskal (apply conj (map (fn [[k v]] (hash-map k (map (fn [[_ id]] [id 1]) v))) (group-by first all-edges))) false)
-        random-edges (map vec (take (/ (count rooms) 2) (shuffle all-edges)))
+(defn rooms-to-edges [nodes]
+  (let [;_ (println "all nodes" nodes)
+        minimum-edges (map #(vector %1 %2) nodes (rest nodes))
+        all-edges (combo/combinations nodes 2)
+        ;_ (println "all edges" all-edges)
+        ;_ (println "minimum-edges" minimum-edges)
+        random-edges (map vec (take (/ (count nodes) 2) (shuffle all-edges)))
         union-edges  (distinct (concat minimum-edges random-edges))]
-    (println minimum-edges)
-    (println union-edges)
-  {:rooms rooms
-   :edges union-edges}))
+    ;(println "union-edges" union-edges)
+    union-edges))
 
 (defn edge-to-points [start-x start-y end-x end-y orientation]
-  (if orientation
-    (let [horz (map-indexed (fn [i e] [(+ start-x i) start-y]) (range (min start-x end-x) (max start-x end-x)))
-          vert (map-indexed (fn [i e] [end-x (+ start-y i)]) (range (min start-y end-y) (max start-y end-y)))
-          points (distinct (concat horz vert [[(max start-x end-x) (max start-y end-y)]]))]
-     points) 
-    (let [horz (map-indexed (fn [i e] [(+ start-x i) end-y]) (range (min start-x end-x) (max start-x end-x)))
-          vert (map-indexed (fn [i e] [start-x (+ start-y i)]) (range (min start-y end-y) (max start-y end-y)))
-          points (distinct (concat vert horz [[(max start-x end-x) (max start-y end-y)]]))]
-     points)))
+  (let [min-x (min start-x end-x)
+        max-x (max start-x end-x)
+        min-y (min start-y end-y)
+        max-y (max start-y end-y)]
+    (if orientation
+      ;; horizontal first
+      (let [horz [(conj (map (fn [x] [x start-y]) (range min-x max-x)) [max-x start-y])]
+            vert (map (fn [y] [[end-x y]]) (range min-y max-y))
+            points (distinct (concat horz vert))]
+       points)
+      ;; vertial first
+      (let [vert (map (fn [y] [[start-x y]]) (range min-y max-y))
+            horz [(conj (map (fn [x] [x end-y]) (range min-x max-x)) [start-x max-y])]
+            points (distinct (concat vert horz))]
+       points))))
 
 (defn points-to-corridor [points]
-  (map (fn [[x y]] [x y {:type :corridor}]) points))
+  (map (fn [line] (map (fn [[x y]] [x y {:type :corridor}]) line)) points))
 
-(defn room-to-cellsxy [min-x min-y max-x max-y]
+(defn room-to-cellsxy  [min-x min-y max-x max-y]
   (let [top    [(map (fn [x] [x min-y {:type :horizontal-wall}]) (range min-x (inc max-x)))]
         bottom [(map (fn [x] [x max-y {:type :horizontal-wall}]) (range min-x (inc max-x)))]
         middle-line (concat [[min-x {:type :vertical-wall}]]
@@ -72,16 +79,78 @@
   {:floor 0
    :horizontal-wall 1
    :vertical-wall 2
-   :nil 3})
+   :corridor 3
+   :nil 4})
+(defn merge-cells [cell1 cell2]
+  (do ;(println "cell1" cell1 "cell2" cell2)
+  (case (cell1 :type)
+    :floor (case (cell2 :type)
+             :floor :floor
+             :horizontal-wall :floor
+             :vertical-wall :floor
+             :corridor :close-door
+             :close-door :floor
+             :up-stairs :up-stairs
+             :down-stairs :down-stairs
+             :nil :floor)
+    :horizontal-wall (case (cell2 :type)
+             :floor :floor
+             :horizontal-wall :horizontal-wall
+             :vertical-wall :horizontal-wall
+             :corridor :close-door
+             :close-door :close-door
+             :up-stairs :up-stairs
+             :down-stairs :down-stairs
+             :nil :horizontal-wall)
+    :vertical-wall (case (cell2 :type)
+             :floor :floor
+             :horizontal-wall :horizontal-wall
+             :vertical-wall :vertical-wall
+             :corridor :close-door
+             :close-door :close-door
+             :up-stairs :up-stairs
+             :down-stairs :down-stairs
+             :nil :vertical-wall)
+    :corridor (case (cell2 :type)
+             :floor :floor
+             :horizontal-wall :close-door
+             :vertical-wall :close-door
+             :corridor :corridor
+             :close-door :close-door
+             :up-stairs :up-stairs
+             :down-stairs :down-stairs
+             :nil :corridor)
+    :close-door (case (cell2 :type)
+             :floor :floor
+             :horizontal-wall :close-door
+             :vertical-wall :close-door
+             :corridor :corridor
+             :close-door :close-door
+             :up-stairs :up-stairs
+             :down-stairs :down-stairs
+             :nil :close-door)
+    :up-stairs (case (cell2 :type)
+             :floor :up-stairs
+             :horizontal-wall :up-stairs
+             :vertical-wall :up-stairs
+             :corridor :up-stairs
+             :close-door :up-stairs
+             :nil :up-stairs)
+    :down-stairs (case (cell2 :type)
+             :floor :down-stairs 
+             :horizontal-wall :down-stairs 
+             :vertical-wall :down-stairs 
+             :corridor :down-stairs 
+             :close-door :down-stairs 
+             :nil :down-stairs)
+    :nil (cell2 :type))))
 
-(defn merge-to-canvas [canvas & cells-xy]
+(defn merge-with-canvas [canvas & cells-xy]
   (let [f (apply comp (map (fn [[x y cell]]
                              (fn [c] (update-in c [y x]
                                (fn [canvas-cell]
-                                 (first (sort-by #(if (nil? %)
-                                                    (cell-type-order :nil)
-                                                    (-> % :type cell-type-order))
-                                                 [cell canvas-cell]))))))
+                                 {:type (merge-cells (if (nil? cell) {:type :nil} cell)
+                                              (if (nil? canvas-cell) {:type :nil} canvas-cell))}))))
                            (apply concat (apply concat cells-xy))))]
    (f canvas)))
 
@@ -91,10 +160,28 @@
         []))
 
 (defn random-place [width height]
-  (let [num-rooms 5
-        rooms     (map (partial make-room 5 5 30 15) (range num-rooms))
-        graph     (rooms-to-graph rooms)]
-    graph))
+  (let [num-rooms 9
+        min-width 3
+        min-height 3
+        max-width 10
+        max-height 4
+        room-bounds  (map (fn [i] (let [x1 (rand-int (- width max-width))
+                                        y1 (rand-int (- height max-height))
+                                        x2 (+ x1 min-width (rand-int (- max-width min-width)))
+                                        y2 (+ y1 min-height (rand-int (- max-height min-height)))]
+                                    [x1 y1 x2 y2]))
+                          (range num-rooms))
+        rooms        (map #(apply room-to-cellsxy %) room-bounds)
+        room-centers (map (fn [[x1 y1 x2 y2]] [(int (/ (+ x1 x2) 2))
+                                               (int (/ (+ y1 y2) 2))])
+                          room-bounds)
+        corridors    (map (fn [[[x1 y1] [x2 y2]]]
+                            (points-to-corridor
+                              (edge-to-points x1 y1 x2 y2 (< (rand-int 2) 1))))
+                          (rooms-to-edges room-centers))
+        upstairs     [[[(conj (first room-centers) {:type :up-stairs})]]]
+        downstairs   [[[(conj (last room-centers) {:type :down-stairs})]]]]
+    (apply merge-with-canvas (canvas width height) (concat corridors rooms upstairs downstairs))))
 
 (defn place-to-ascii [place]
   (let [contents (map (fn [line] (apply str
@@ -105,15 +192,15 @@
                                               :floor \.
                                               :vertical-wall \|
                                               :horizontal-wall \-
-                                              \?)))
+                                              :close-door \+
+                                              :corridor \#
+                                              :up-stairs \<
+                                              :down-stairs \>
+                                              (str (cell :type)))))
                                        line))) place)]
     contents))
 
 (defn -main [& args]
   (println "generating...")
-  (doall (map println (place-to-ascii (merge-to-canvas (canvas 20 20)
-                                                       (room-to-cellsxy 1 1 8 4)
-                                                       (room-to-cellsxy 6 3 10 19)
-                                                       (room-to-cellsxy 2 10 15 15)))))
-  (println (random-place 30 20)))
+  (doall (map println (place-to-ascii (random-place 55 20)))))
 

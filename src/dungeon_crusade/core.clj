336de62b74f29c3740eed0cb706abb07eef715c5
Kelsey Sorrels <kelsey.d.sorrels@gmail.com>
2014-04-05 21:10:53 -0700

    Make line of sight VERY fast

--- src/dungeon_crusade/core.clj
+++ src/dungeon_crusade/core.clj
@@ -1,14 +1,17 @@
 (ns dungeon-crusade.core
   (:use dungeon-crusade.main
-        clojure.stacktrace))
+        clojure.stacktrace)
+  (:require
+        dungeon-crusade.lineofsight))
 
 (defn -main []
-  ; start with initial state from setup-fn
-  (loop [state (setup)]
-    (if (nil? state)
-      (System/exit 0))
-    ; tick the old state through the tick-fn to get the new state
-    (recur (try (tick state)
-      (catch Exception ex
-        (do (print-stack-trace ex)
-            (throw ex)))))))
+  (do
+    ; start with initial state from setup-fn
+    (loop [state (setup)]
+      (if (nil? state)
+        (System/exit 0))
+      ; tick the old state through the tick-fn to get the new state
+      (recur (try (tick state)
+        (catch Exception ex
+          (do (print-stack-trace ex)
+              (throw ex))))))))

b145ce1ddcc06c25709d4ab5e51e13e6d2f21a25
Kelsey Sorrels <kelsey.d.sorrels@gmail.com>
2014-12-16 13:42:54 -0800

    Fix log scrolling - again

--- test/robinson/update_test.clj
+++ test/robinson/update_test.clj
@@ -2,11 +2,18 @@
   (:use clojure.test
         robinson.common
         robinson.world
-        robinson.update))
+        robinson.update)
+  (:require [taoensso.timbre :as tm]))
+
+
+(defn inc-time [state] (update-in state [:world :time] inc))
 
 (deftest more-log
-  (let [state {:world {:time 10 :log []}}
-        state (-> state
+  (let [state (-> {:world {:time 10 :log []}}
+                (append-log "first message...........................................................................")
+                inc-time
+                (append-log "second message..........................................................................")
+                inc-time
                 (append-log "first message")
                 (append-log "second message")
                 (append-log "third message")
@@ -19,6 +26,7 @@
     (is (current-state state) :more-log)
     (is logs-viewed 1)
     (is message "first message second message third message")
+    (tm/info "Saw message:" message)
     (let [state (scroll-log state)
           logs-viewed (get-in state [:world :logs-viewed])
           logs        (filter #(= (get % :time) (get-time state)) (get-in state [:world :log]))
@@ -26,12 +34,14 @@
       (is (current-state state) :more-log)
       (is logs-viewed 2)
       (is message "fourth lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnng message")
+      (tm/info "Saw message:" message)
       (let [state (scroll-log state)
             logs-viewed (get-in state [:world :logs-viewed])
             logs        (filter #(= (get % :time) (get-time state)) (get-in state [:world :log]))
             message     (nth logs (dec logs-viewed))]
         (is (current-state state) :normal)
         (is logs-viewed 3)
-        (is message "fifth message")))))
+        (is message "fifth message")
+        (tm/info "Saw message:" message)))))
 
 

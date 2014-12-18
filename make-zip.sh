4bdb5da98e86fd19f409fb7804a6395eba7f46cb
Aaron Santos <aaronds109@gmail.com>
2014-12-17 21:26:02 -0800

    Add version tracking to build script

--- make-zip.sh
+++ make-zip.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+#clean
 rm target/robinson-win.zip
 rm -rf target/robinson-win
 mkdir -p target/robinson-win
 
+# keep track of the build version
+git rev-parse HEAD >> target/robinson-win/VERSION
+
+#copy jar
 cp target/robinson-0.0.1-SNAPSHOT-standalone.jar target/robinson-win/robinson.jar
 chmod +x target/robinson-win/robinson.jar
 
+#copy resources
 cp -R config target/robinson-win/
 cp -R data   target/robinson-win/
 cp -R images target/robinson-win/
@@ -14,6 +20,7 @@ cp -R quests target/robinson-win/
 mkdir     target/robinson-win/log
 mkdir     target/robinson-win/save
 
+# make zip
 cd target
 zip -r robinson-win robinson-win
 cd -

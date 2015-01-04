3a338c726180a00f7fb149a45c7a00a458a8a7ac
Aaron Santos <aaronds109@gmail.com>
2015-01-03 16:57:50 -0800

    Add some common tests

--- test/robinson/common_test.clj
+++ test/robinson/common_test.clj
@@ -1,15 +1,50 @@
 (ns robinson.common-test
   (:use clojure.test
-        robinson.common
-        robinson.world))
+        robinson.common))
 
-(deftest test-adjacent-to-player-0
-  (is (false? (adjacent-to-player? {:world {:player {:pos {:x 26, :y 9}}}}
-                                  {:x 34 :y 7}))))
+(deftest arg-when->test
+  (are [in out] (= (-> in
+                     (arg-when-> [x] (> x 3)
+                       (* 3)))
+                   out)
+    0 0
+    1 1
+    2 2
+    3 3
+    4 12
+    5 15))
 
-(deftest test-adjacent-to-player-1
-  (is (true? (adjacent-to-player? {:world {:player {:pos {:x 26, :y 9}}}}
-                                  {:x 25 :y 9}))))
+(deftest arg-if->test
+  (are [in out] (= (-> in
+                     (arg-if-> [x] (> x 3)
+                       (* 3)
+                       (* 2)))
+                   out)
+    0 0
+    1 2
+    2 4
+    3 6
+    4 12
+    5 15))
+
+(deftest pos->xy-test
+  (are [in out] (= (pos->xy in) out)
+    {:x -1 :y -2} [-1 -2]
+    {:x 0.0 :y 2} [0.0 2]))
+
+(deftest xy->pos-test
+  (are [in out] (= (apply xy->pos in) out)
+    [-1 -2] {:x -1 :y -2}
+    [0.0 2] {:x 0.0 :y 2}))
+
+(deftest concat-in-test
+  (are [m ks v out] (= (concat-in m ks v) out)
+    {:a {:b {:c [:d :e :f]}}} [:a :b :c] [:g :h] {:a {:b {:c [:d :e :f :g :h]}}}))
+
+(deftest reduce-in-test
+  (are [m ks f v out] (= (reduce-in m ks f v) out)
+    {:a {:b {:c [1 2 3]}}} [:a :b :c] + 0 {:a {:b {:c 6}}}
+    {:a {:b {:c [1 2 3]}}} [:a :b :c] + 1 {:a {:b {:c 7}}}))
 
 (deftest map-indexed-in-p-0
   (is (= (map-indexed-in-p {:a [5 4 3 2 1]} [:a] even? (fn [idx item] idx))
@@ -22,66 +57,8 @@
                             (fn [item] (= (item :type) :$)))
            expected))))
 
-(def direction->cells-state
- {:world {:viewport {:width 6
-                     :height 5}
-          :width 6
-          :height 5
-          :player {:pos {:x 2 :y 2}}
-          :places {[0 0] [[:a :b :c :d :e :f]
-                          [:g :h :i :j :k :l]
-                          [:m :n :o :p :q :r]
-                          [:s :t :y :v :w :x]
-                          [:y :z :0 :1 :2 :3]]}}})
 
-(deftest direction->cells-0
-  (is (= (direction->cells
-            direction->cells-state
-            :left
-            2)
-         [:n :m])))
-
-(deftest direction->cells-1
-  (is (= (direction->cells
-            direction->cells-state
-            :right
-            3)
-         [:p :q :r])))
-
-(deftest direction->cells-2
-  (is (= (direction->cells
-            direction->cells-state
-            :up
-            2)
-         [:i :c])))
-
-(deftest direction->cells-3
-  (is (= (direction->cells
-            direction->cells-state
-            :down
-            2)
-         [:y :0])))
-          
-
-(def first-collidable-cells-state
- {:world {:viewport {:width 5
-                     :height 5}
-          :width 5
-          :height 5
-          :player {:pos {:x 2 :y 2}}
-          :npcs []
-          :places {[0 0] [[{:type :vertical-wall}  {:type :vertical-wall}{:type :vertical-wall}{:type :vertical-wall}{:type :vertial-wall}]
-                          [{:type :horizontal-wall}{:type :floor}        {:type :floor}        {:type :floor}        {:type :horizontal-wall}]
-                          [{:type :horizontal-wall}{:type :floor}        {:type :floor}        {:type :floor}        {:type :horizontal-wall}]
-                          [{:type :horizontal-wall}{:type :floor}        {:type :floor}        {:type :floor}        {:type :horizontal-wall}]
-                          [{:type :vertical-wall}  {:type :vertical-wall}{:type :vertical-wall}{:type :vertical-wall}{:type :vertial-wall}]]}}})
-
-(deftest first-collidable-cells-0
-  (is (= {:cell {:type :vertical-wall} :pos {:x 2 :y 0}}
-         (let [state first-collidable-cells-state] 
-           (first-collidable-object state :up 3)))))
-
-(deftest farther-than
+(deftest farther-than-test
   (are [p1 p2 d r] (= (farther-than? p1 p2 d) r)
   {:x 0 :y 0}  {:x 1 :y 1}  1 true
   {:x 0 :y 0}  {:x 1 :y 1}  1.1 true

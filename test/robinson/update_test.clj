3affd95aa3a6772a428934a23c9d06e6244bd371
Aaron Santos <aaronds109@gmail.com>
2015-01-03 21:41:27 -0800

    Add pick-up test

--- test/robinson/update_test.clj
+++ test/robinson/update_test.clj
@@ -1,47 +1,38 @@
 (ns robinson.update-test
   (:use clojure.test
         robinson.common
-        robinson.world
-        robinson.update)
-  (:require [taoensso.timbre :as tm]))
+        robinson.update))
+
+(def test-state-0
+ {:world {:seed 0
+          :viewport {:width 5
+                     :height 5
+                     :pos {:x 0 :y 0}}
+          :width 5
+          :height 5
+          :volcano-pos {:x 100 :y 100}
+          :volcano-xys []
+          :player {:pos {:x 2 :y 2}
+                   :inventory []}
+          :npcs []
+          :places {[0 0] [[{:type :vertical-wall}  {:type :vertical-wall}{:type :vertical-wall}{:type :vertical-wall}{:type :vertial-wall}]
+                          [{:type :horizontal-wall}{:type :floor}        {:type :floor}        {:type :floor}        {:type :horizontal-wall}]
+                          [{:type :horizontal-wall}{:type :floor}        {:type :floor}        {:type :floor}        {:type :horizontal-wall}]
+                          [{:type :horizontal-wall}{:type :floor}        {:type :floor}        {:type :floor}        {:type :horizontal-wall}]
+                          [{:type :vertical-wall}  {:type :vertical-wall}{:type :vertical-wall}{:type :vertical-wall}{:type :vertial-wall}]]}}})
+
+(deftest first-collidable-cells-0
+  (let [state test-state-0
+        state (move state :up)]
+    (is (= (get-in state [:world :player :pos]) {:x 2 :y 1}))))
 
 
-(defn inc-time [state] (update-in state [:world :time] inc))
-
-(deftest more-log
-  (let [state (-> {:world {:time 10 :log []}}
-                (append-log "first message...........................................................................")
-                inc-time
-                (append-log "second message..........................................................................")
-                inc-time
-                (append-log "first message")
-                (append-log "second message")
-                (append-log "third message")
-                (append-log "fourth lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnng message")
-                (append-log "fifth message"))
-        state (init-log-scrolling state)
-        logs-viewed (get-in state [:world :logs-viewed])
-        logs        (filter #(= (get % :time) (get-time state)) (get-in state [:world :log]))
-        message     (nth logs (dec logs-viewed))]
-    (is (current-state state) :more-log)
-    (is logs-viewed 1)
-    (is message "first message second message third message")
-    (tm/info "Saw message:" message)
-    (let [state (scroll-log state)
-          logs-viewed (get-in state [:world :logs-viewed])
-          logs        (filter #(= (get % :time) (get-time state)) (get-in state [:world :log]))
-          message     (nth logs (dec logs-viewed))]
-      (is (current-state state) :more-log)
-      (is logs-viewed 2)
-      (is message "fourth lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnng message")
-      (tm/info "Saw message:" message)
-      (let [state (scroll-log state)
-            logs-viewed (get-in state [:world :logs-viewed])
-            logs        (filter #(= (get % :time) (get-time state)) (get-in state [:world :log]))
-            message     (nth logs (dec logs-viewed))]
-        (is (current-state state) :normal)
-        (is logs-viewed 3)
-        (is message "fifth message")
-        (tm/info "Saw message:" message)))))
-
-
+(deftest pick-up-0
+  (let [state     test-state-0
+        state     (-> state
+                    (assoc-in [:world :remaining-hotkeys] [\a])
+                    (assoc-in [:world :places [0 0] 2 2 :items] [{:id :test-item}])
+                    (assoc-in [:world :selected-hotkeys] #{\a}))
+        new-state (pick-up state)]
+    (is (= (get-in new-state [:world :player :inventory 0 :id]) :test-item))
+    (is (= (get-in new-state [:world :player :inventory 0 :hotkey]) \a))))

1955f314eb93b94c478861da18718c1c2c5d84ee
Aaron Santos <aaronds109@gmail.com>
2015-02-08 22:06:48 -0800

    Add project debugging

--- project.clj
+++ project.clj
@@ -78,6 +78,8 @@
   :repl-options {:timeout 920000}
   :jvm-opts [
              ;"-agentpath:/home/santos/bin/yjp-2014-build-14096/bin/linux-x86-64/libyjpagent.so"
+             "-Xdebug"
+             "-Xrunjdwp:transport=dt_socket,server=y,suspend=n"
              "-XX:+UseParNewGC"
              "-XX:+UseConcMarkSweepGC"
              "-XX:+CMSConcurrentMTEnabled"

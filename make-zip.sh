b3820c011d08325d013d00655eabaaa4a1144999
Aaron Santos <aaronds109@gmail.com>
2015-07-25 01:20:53 -0400

    Update uberjar/zip build

--- make-zip.sh
+++ make-zip.sh
@@ -1,30 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
-#clean
-rm target/robinson-win.zip
-rm -rf target/robinson-win
-mkdir -p target/robinson-win
+LEINUBERJAR=0
+UPLOAD=0
+while getopts "lu" opt;
+do
+    echo "Got opt $opt"
+    case "$opt" in
+    l) LEINUBERJAR=1;;
+    u) UPLOAD=1;;
+    esac
+done
 
 # keep track of the build version
-git rev-parse HEAD >> target/robinson-win/VERSION
+VERSION=`date +'%Y.%m.%d-%H.%M.%S'`'-'`git rev-parse --short HEAD`
+
+#clean
+rm target/robinson-*.zip
+#rm -rf target/robinson-*
+
+if [[ $LEINUBERJAR == 1 ]]; then
+  lein uberjar
+fi
+
+#setup target dir
+mkdir -p target/robinson-$VERSION
 
 #copy jar
-cp target/robinson-0.0.1-SNAPSHOT-standalone.jar target/robinson-win/robinson.jar
-chmod +x target/robinson-win/robinson.jar
+cp target/robinson-0.0.1-SNAPSHOT-standalone.jar target/robinson-$VERSION/robinson.jar
+chmod +x target/robinson-$VERSION/robinson.jar
 
 #copy resources
-cp -R config target/robinson-win/
-cp -R data   target/robinson-win/
-cp -R images target/robinson-win/
-cp -R quests target/robinson-win/
-mkdir     target/robinson-win/log
-mkdir     target/robinson-win/save
+echo $VERSION >> target/robinson-$VERSION/VERSION
+cp -R config target/robinson-$VERSION/
+cp -R data   target/robinson-$VERSION/
+cp -R images target/robinson-$VERSION/
+cp -R quests target/robinson-$VERSION/
+mkdir     target/robinson-$VERSION/log
+mkdir     target/robinson-$VERSION/save
 
 # set configuration to upload saves
-touch target/robinson-win/config/.feedbackparticipant
+touch target/robinson-$VERSION/config/.feedbackparticipant
 
 # make zip
 cd target
-zip -r robinson-win robinson-win
+zip -r robinson-$VERSION.zip robinson-$VERSION
 cd -
+
+echo "Wrote target/robinson-$VERSION.zip"
+
+if [[ $UPLOAD == 1 ]] ; then
+  s3cmd put target/robinson-$VERSION.zip s3://aaron-santos.com.robinson/download/zip/robinson-$VERSION.zip
+fi
+
 

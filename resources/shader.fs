e9008fa47c840bb4d0e37fd54a7f65745188c2ae
Aaron Santos <aaronds109@gmail.com>
2016-01-28 22:31:36 -0800

    Switch to integer texture for glyph lookup

--- resources/shader.fs
+++ resources/shader.fs
@@ -1,8 +1,11 @@
-precision mediump float;
-uniform sampler2D uFont, uGlyphs, uFg, uBg;
+#version 330
+
+uniform sampler2D uFont, uFg, uBg;
+uniform isampler2D uGlyphs;
 uniform vec2 fontSize, termDimensions, fontTextureDimensions, glyphTextureDimensions;
 
-varying vec2 vTextureCoord;
+in vec2 vTextureCoord;
+out vec4 outcolor;
 
 void main(void) {
   // width and height of the screen in pixels
@@ -10,28 +13,28 @@ void main(void) {
   float screenHeight = fontSize.y * termDimensions.y;
   // termDimensions in cols and rows
   // (termCol, termRow) = (col, row) the fragment belongs to in the terminal
-  float termCol = floor(vTextureCoord.x * termDimensions.x);
-  float termRow = floor(vTextureCoord.y * termDimensions.y);
+  ivec2 termXY = ivec2(floor(vTextureCoord.x * termDimensions.x),
+                       floor(vTextureCoord.y * termDimensions.y));
   // row,column -> glyph texture uvs
-  float termu = (termCol / glyphTextureDimensions.x);
-  float termv = (termRow / glyphTextureDimensions.y);
+  //float termu = (termCol / glyphTextureDimensions.x);
+  //float termv = (termRow / glyphTextureDimensions.y);
   // look up in uGlphs the index in the font texture to use for the fragment
   // fontIndex.x = font index 
-  vec2 fontIndex = texture2D(uGlyphs, vec2(termu, termv)).xy;
+  ivec2 fontIndex = texelFetch(uGlyphs, termXY, 0).xy;
   // That green and red channel mingling? Yeah that's because some
   // mobile devices have less than 8 bits per channel
   //float gp = floor(glyph.r * 256.0 + floor((glyph.g * 16.0) * 256.0)) / 16.0;
   //float gy = 0.9375 - (floor(gp) / 16.0);
   //float gx = gp - floor(gp);
   // calc the position of the fragment relative to the terminal cell
-  float charu = fract(vTextureCoord.x * termDimensions.x);
-  float charv = fract(vTextureCoord.y * termDimensions.y);
-  float fontu = ((fontIndex.x * fontTextureDimensions.x) + charu) * fontSize.x / fontTextureDimensions.x;
-  float fontv = ((fontIndex.y * fontTextureDimensions.y - charv + 1) * fontSize.y)/ fontTextureDimensions.y;
-  vec4 fnt = texture2D(uFont, vec2(fontu, fontv));
+  ivec2 charXY = ivec2(fract(vTextureCoord.x * termDimensions.x),
+                       fract(vTextureCoord.y * termDimensions.y)) * ivec2(fontSize.x, fontSize.y);
+  //float fontu = ((fontIndex.x * fontTextureDimensions.x) + charu) * fontSize.x / fontTextureDimensions.x;
+  //float fontv = ((fontIndex.y * fontTextureDimensions.y - charv + 1) * fontSize.y)/ fontTextureDimensions.y;
+  vec4 fnt = texelFetchOffset(uFont, fontIndex, 0, charXY);
 
-  vec4 fg  = texture2D(uFg, vec2(termu, termv));
-  vec4 bg  = texture2D(uBg, vec2(termu, termv));
+  vec4 fg  = texelFetch(uFg, termXY, 0);
+  vec4 bg  = texture2D(uBg, termXY, 0);
   //vec4 fg  = texture2D(uFg, vec2(termu, termv));
   //vec4 bg  = texture2D(uBg, vec2(termu, termv));
   //gl_FragColor = vec4(vTextureCoord.s, vTextureCoord.t, 1.0, 1.0);
@@ -46,6 +49,6 @@ void main(void) {
 
   //gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
   // Final color
-  gl_FragColor = mix(bg, fg, fnt.r);
+  outcolor = mix(bg, fg, fnt.r);
 
 }

351bf771eff323efbd36e4db12900d83ad49ba2b
Kelsey Sorrels <kelsey.d.sorrels@gmail.com>
2016-01-29 22:41:38 -0800

    Fix default background color

--- resources/shader.fs
+++ resources/shader.fs
@@ -47,6 +47,7 @@ void main(void) {
   //outcolor = vec4(fontIndex.x, fontIndex.y, 0, 0) / 5120000000.0;
   //outcolor = (vec4(fontIndex.x, fontIndex.y, 0, 0) + vec4(charXY.x, charXY.y, 0, 0)) / 512.0;
   //outcolor = fg;
+  //outcolor = bg;
 
   //gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
   // Final color

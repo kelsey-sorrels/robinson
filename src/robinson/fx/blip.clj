bbf1d6a627e67d213750dea99c173cc4cd8da732
Aaron Santos <aaronds109@gmail.com>
2018-11-14 22:06:22 -0800

    Add fs and fx namespaces

---
+++ src/robinson/fx/blip.clj
@@ -0,0 +1,39 @@
+;; Utility functions for rendering state
+(ns robinson.fx.blip
+  (:require 
+            [robinson.common :as rc]
+            [robinson.math :as rmath]
+            [robinson.color :as rcolor]
+            [robinson.actors :as ractors]
+            [robinson.fx :as rfx]
+            [robinson.world :as rw]
+            [taoensso.timbre :as log]))
+
+
+(defrecord BlipActor [ttl fx-ks]
+  ractors/Actor
+  (receive [this state]
+    (let [ttl-zero (or (zero? ttl) (neg? ttl))]
+      (letfn [(cleanup [state]
+                (-> state
+                  (ractors/remove-actor this)
+                  (rc/dissoc-in fx-ks)))]
+        (if ttl-zero
+          ; if done, cleaup
+          (cleanup state)
+          ; else, dec ttl
+          (ractors/update-actor state this :ttl dec))))))
+
+(defmethod rfx/conj-effect :blip [state fx-type & [pos ch fg bg ttl]]
+  (let [fx-id (rfx/fx-id)
+        fg (or fg [255 255 255 255])
+        bg (or bg [0 0 0 0])
+        ttl (or ttl 1)
+        actor (->BlipActor ttl (rfx/fx-ks fx-id))]
+    (log/info "created BlipActor " ttl (rfx/fx-ks fx-id))
+    (-> state
+      ; create a character fx
+      (rfx/conj-fx (rfx/character-fx ch pos fg bg) fx-id)
+      ; create a corresponding actor that controls the fx
+      (ractors/add-actor actor))))
+
